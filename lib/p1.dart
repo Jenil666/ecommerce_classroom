1a3b2f7ae31fb2645fc127b9a56773c98a6dde90
Jenil666 <jenilchandarana33>
2023-01-15 19:30:05 +0530

    15-1-23 7:29

--- lib/p1.dart
+++ lib/p1.dart
@@ -12,7 +12,23 @@ class _P1State extends State<P1> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold(
-      appBar: AppBar(title: Text("Airpode pro",style: TextStyle(color: Colors.black),),centerTitle: true,),
+      appBar: AppBar(title: Text("Airpode pro",style: TextStyle(color: Colors.white),),centerTitle: true,),
+      body: Column(
+        children: [
+          Expanded(flex:1,child: Container(
+            child: Image.asset("asset/image/airpods.jpg",height: 300,width: 300,),
+          )),
+          Expanded(flex:1,child: Container(
+            child: Column(
+              children: [
+                Text("Apple",style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontSize: 30),),
+                Text("\$249",style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontSize: 30),),
+              ],
+            ),
+          )),
+
+        ],
+      ),
     ),
     );
   }
